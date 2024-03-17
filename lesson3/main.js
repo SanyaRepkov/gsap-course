18a55036686ea03422564383c7609cfdca158f3c
SanyaRepkov <66551405+SanyaRepkov@users.noreply.github.com>
2024-03-17 16:31:04 +0300

    Update ls3

--- lesson3/main.js
+++ lesson3/main.js
@@ -1,5 +1,5 @@
-let titleProject = TenThousandDollarsProject
-let screensValue = "шаблонные, с уникальным дизайном, с анимациями"
+let titleProject = 'TenThousandDollarsProject'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
 let screenPrice = 3333
 let percentage = 1
 let fullPrice = 100000
