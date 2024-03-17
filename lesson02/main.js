9d196e34025c87fd133b821f573c9543273f90cc
SanyaRepkov <66551405+SanyaRepkov@users.noreply.github.com>
2024-03-17 17:18:33 +0300

    Add files via upload

---
+++ lesson02/main.js
@@ -0,0 +1,6 @@
+let titleProject = TenThousandDollarsProject
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями"
+let screenPrice = 3333
+let percentage = 1
+let fullPrice = 100000
+let responsive = true
