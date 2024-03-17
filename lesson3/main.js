ce3d4d3e2b62a2de9905370f8660ec21b9d86218
SanyaRepkov <66551405+SanyaRepkov@users.noreply.github.com>
2024-03-17 17:15:45 +0300

    Delete lesson3 directory

--- lesson3/main.js
+++
@@ -1,6 +0,0 @@
-let titleProject = 'TenThousandDollarsProject'
-let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-let screenPrice = 3333
-let percentage = 1
-let fullPrice = 100000
-let responsive = true
