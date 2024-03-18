c53b6f43f7cbd366ec0475e2dce067f58e184c04
SanyaRepkov <66551405+SanyaRepkov@users.noreply.github.com>
2024-03-18 09:15:15 +0300

    Update lesson-04.js

--- lesson04/lesson-04.js
+++ lesson04/lesson-04.js
@@ -12,51 +12,56 @@ let service2 = prompt('Какой еще сервис тебе нужен?');
 console.log(service2);
 let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?');
 console.log(servicePrice2);
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
-let servicePercentPrice = fullPrice - (fullPrice * 0.15);
-servicePercentPrice = Math.round(servicePercentPrice);
-if (fullPrice > 50000) {
+// let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+// console.log(fullPrice);
+// let servicePercentPrice = fullPrice - (fullPrice * 0.15);
+// servicePercentPrice = Math.round(servicePercentPrice);
+
+
+let getAllServicePrices = function(serv1, serv2) {
+  return serv1 + serv2;
+};
+
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+function getFullPrice(screenPrice) {
+  return screenPrice + allServicePrices;
+}
+
+let fullPrice = getFullPrice(screenPrice);
+
+function getTitle(titleProject) {
+  return titleProject.trim().charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
+}
+
+
+titleProject = getTitle(titleProject);
+console.log(titleProject);
+
+function getServicePercentPrice(fullPrice, percent) {
+  return Math.round(fullPrice - (fullPrice * (percent / 100)));
+}
+let servicePercentPrice = getServicePercentPrice(fullPrice, 15);
+console.log(servicePercentPrice);
+
+const getRollbackMessage = (price) => {
+  if (price > 50000) {
     console.log("Сделаем скидку 10%");
-  } else if (fullPrice > 20000 && fullPrice <= 50000) {
+  } else if (price > 20000 && price <= 50000) {
     console.log("Сделаем скидку 5%");
-  } else if (fullPrice > 0 && fullPrice <= 20000) {
+  } else if (price > 0 && price <= 20000) {
     console.log("Скидка не предусмотрена");
-  } else if (fullPrice === 0) {
+  } else if (price === 0) {
     console.log("Цена равна 0");
-  } else if (fullPrice === 20000) {
+  } else if (price === 20000) {
     console.log("Цена равна 20000");
-  } else if (fullPrice === 50000) {
+  } else if (price === 50000) {
     console.log("Цена равна 50000");
   } else {
     console.log("Что-то пошло не так");
   }
-console.log(servicePercentPrice);
 
-let getAllServicePrices = function(serv1, serv2) {
-  let allServicePrices = serv1 + serv2;
-  console.log(allServicePrices);
-};
-
-getAllServicePrices(servicePrice1, servicePrice2);
-
-function getFullPrice(screenPrice, servicePrice1, servicePrice2) {
-  let allServicePrices = servicePrice1 + servicePrice2;
-  return screenPrice + allServicePrices;
+  console.log(servicePercentPrice);
 }
 
-let fullPrice2 = getFullPrice(screenPrice, servicePrice1, servicePrice2);
-console.log(fullPrice2);
-
-function getTitle(titleProject) {
-  return titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
-}
-
-let editedTitle = getTitle(titleProject);
-console.log(editedTitle);
-
-function getServicePercentPrice(fullPrice, percent) {
-  return fullPrice - (fullPrice * (percent / 100));
-}
-servicePercentPrice = getServicePercentPrice(fullPrice, 15);
-console.log(servicePercentPrice);
+getRollbackMessage(fullPrice);
